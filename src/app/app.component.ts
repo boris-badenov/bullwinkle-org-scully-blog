40bdd5eec798524746878dc5d34c710bdc07e348
Boris Badenov <badenov196@gmail.com>
2021-03-11 04:52:11 +0300

    scully configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scully-blog';
+
+  isAdminPage$: Observable<boolean>;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.isAdminPage$ = this.router.events.pipe(
+      filter((evt: any) => {
+        return evt instanceof NavigationEnd;
+      }),
+      map((evt: NavigationEnd) => {
+        return evt.url.includes('/admin');
+      }),
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
