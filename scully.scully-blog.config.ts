096f57f3f52146ba002778daac0eb7f2342b927c
Boris Badenov <badenov196@gmail.com>
2021-03-11 04:24:40 +0300

    scullyio/init

---
+++ scully.scully-blog.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "scully-blog",
+  outDir: './dist/static',
+  routes: {
+  }
+};
